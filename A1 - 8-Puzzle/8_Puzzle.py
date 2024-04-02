a6899b97037c596aeb2b5157a0d260967106aaf3
Joao Luis Rosa de Moura <Joao.Moura@zaffari.com.br>
2024-04-02 17:42:17 -0300

    2024-04-02 Continuação do algoritmo de a*

--- A1 - 8-Puzzle/8_Puzzle.py
+++ A1 - 8-Puzzle/8_Puzzle.py
@@ -1,7 +1,7 @@
 def printaMatriz(matriz):
     for linha in matriz:
-        for valor in linha:
-            print(valor, end=' ')
+        for coluna in linha:
+            print(coluna, end=' ')
         print()
 
 def calculaHeuristica(matriz):
@@ -11,18 +11,54 @@ def calculaHeuristica(matriz):
     for linha in matriz:
         for valor in linha:
             posicao += 1
-            # print (posicao)
             if valor == posicao:
                 acertos += 1
             else:
                 erros += 1
-    erros = erros + (posicao - acertos)
-    return (erros - acertos)
+    heuristica = posicao - acertos + erros
+    return heuristica
 
 
-estado_inicial = [[1, 0, 3], [4, 2, 5], [7, 8, 6]]
-estado_final = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-printaMatriz(estado_inicial)
+def testePossiveisMovimentos(estado_inicial):
+    possibilidades = []
+    for linha in range(len(estado_inicial)):
+        for coluna in range(len(estado_inicial[linha])):
+            if estado_inicial[linha][coluna] == 0:             
+                if linha > 0:
+                    estado = [row[:] for row in estado_inicial]
+                    estado[linha][coluna], estado[linha-1][coluna] = estado[linha-1][coluna], estado[linha][coluna]
+                    possibilidades.append(estado)
+                
+                
+                if linha < len(estado_inicial) - 1:
+                    estado = [row[:] for row in estado_inicial]
+                    estado[linha][coluna], estado[linha+1][coluna] = estado[linha+1][coluna], estado[linha][coluna]
+                    possibilidades.append(estado)
 
-print(calculaHeuristica(estado_inicial))
-printaMatriz(estado_inicial)
+                
+                if coluna > 0:
+                    estado = [row[:] for row in estado_inicial]
+                    estado[linha][coluna], estado[linha][coluna-1] = estado[linha][coluna-1], estado[linha][coluna]
+                    possibilidades.append(estado)
+
+                
+                if coluna < len(estado_inicial[linha]) - 1:
+                    estado = [row[:] for row in estado_inicial]
+                    estado[linha][coluna], estado[linha][coluna+1] = estado[linha][coluna+1], estado[linha][coluna]
+                    possibilidades.append(estado)
+    return possibilidades
+
+
+
+
+inicioJogo = [[1, 0, 3], [4, 2, 5], [7, 8, 6]]
+fimJogo = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+
+for possibilidade in testePossiveisMovimentos(inicioJogo):
+    printaMatriz(possibilidade)
+    print(calculaHeuristica(possibilidade))
+    print()
+
+estado = [row[:] for row in inicioJogo]
+print(estado)
---
+++ A1 - 8-Puzzle/teste.py
@@ -0,0 +1,6 @@
+def printaMatriz(matriz):
+    for linha in matriz:
+        for valor in linha:
+            print(valor, end=' ')
+        print()
+
